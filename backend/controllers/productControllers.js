b86bebb7d0a767f141bd29c0cc17c89b6ce49422
Sayuru Dev <mailtosayuru@gmail.com>
2025-07-13 01:51:39 -0400

    admin backend

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,6 @@
 import Product from "../models/product.js";
 
+// Create new Product   =>  /api/v1/products
 export const getProducts = async (req, res) => {
   const products = await Product.find();
 
@@ -8,6 +9,7 @@ export const getProducts = async (req, res) => {
   });
 };
 
+// Create new Product   =>  /api/v1/admin/products
 export const newProduct = async (req, res) => {
   const product = await Product.create(req.body);
 
@@ -16,6 +18,41 @@ export const newProduct = async (req, res) => {
   });
 };
 
+// Get single product details   =>  /api/v1/products/:id
+export const getProductDetails = async (req, res) => {
+  const product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    product,
+  });
+};
+
+// Update product details   =>  /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({
+    product,
+  });
+};
+
+// Delete product   =>  /api/v1/products/:id
 export const deleteProduct = async (req, res) => {
   const product = await Product.findById(req?.params?.id);
 
